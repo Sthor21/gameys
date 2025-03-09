123da0f093382fda6752f95848adfd4204ca8c1f
Shashank.S <shashankshivu4@gmail.com>
2025-03-09 11:42:19 +0530

    fixing routing issues

--- gamey/src/App.js
+++ gamey/src/App.js
@@ -5,26 +5,28 @@ import Tenzies from "./Pages/Tenzies/Tenzies";
 import Hangman from "./Pages/Hangman/Hangman";
 import MemoryGame from "./Pages/MemoryGame/MemoryGame";
 import Contact from "./Pages/Contact/Contact";
+
+function RedirectToGit() {
+  useEffect(() => {
+    window.location.href = "https://github.com/Sthor21"; // External URL
+  }, []);
+
+  return null; // No need to render anything
+}
+
 function App() {
-    function RedirectToGit() {
-        useEffect(() => {
-          window.location.href = "https://github.com/Sthor21"; // External URL
-        }, []);
-      
-        return null; // No need to render anything
-      }
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/tenzies" element={<Tenzies />} />
-                <Route path="/hangman" element={<Hangman />} />
-                <Route path="/memorygame" element={<MemoryGame />} />
-                <Route path="/contribute" element={<RedirectToGit />}/>
-                <Route path="/contact" element={<Contact/>}/>
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router basename="/gameys"> {/* Added basename for GitHub Pages */}
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/tenzies" element={<Tenzies />} />
+        <Route path="/hangman" element={<Hangman />} />
+        <Route path="/memorygame" element={<MemoryGame />} />
+        <Route path="/contribute" element={<RedirectToGit />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
